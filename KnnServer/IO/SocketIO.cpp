3c66d60a50dc55cb469f225cabe5fd28c85cc304
Gal Pearl <galpearl24@gmail.com>
2023-01-12 19:16:19 +0200

    Add files via upload
    Manual upload, could not push automatically

---
+++ KnnServer/IO/SocketIO.cpp
@@ -0,0 +1,25 @@
+#include "SocketIO.h"
+//TODO: read any data: options and lines
+string SocketIO::read() {
+    string msg;
+    string ending = "\0";
+    //Read until ending is detected
+    char buffer[2048];
+    ssize_t read_bytes;
+    buffer[0] = 0;
+    read_bytes = recv(this->client_sock, buffer, sizeof(buffer), 0);
+    if (read_bytes < 0) {
+        perror("error receiving from client");
+    }
+    msg.append(buffer);
+    msg.append(ending);
+    return msg;
+}
+void SocketIO::write(string s) {
+    std::string msg = s + "\0";
+    //Send the string through the socket
+    size_t sent_bytes = send(this->client_sock, msg.c_str(), strlen(msg.c_str()), 0);
+    if (sent_bytes < 0) {
+        perror("error sending to client");
+    }
+}
