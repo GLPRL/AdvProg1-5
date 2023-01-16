8c509f58347b2a1495b6e10a3b21a6c6ebadb30d
GLPRL <galpearl24@gmail.com>
2023-01-16 18:38:49 +0200

    new line at error print

--- KnnServer/Command/SendnSaveData.h
+++ KnnServer/Command/SendnSaveData.h
@@ -10,14 +10,14 @@ public:
     void execute() override {
         string s;
         if (this->getCd()->getV().size() == 0) {            //If no test vector exists
-            s = "please upload data";
+            s = "please upload data\n";
             this->getIO()->write(s);
             s = "@";
             this->getIO()->write(s);
             return;
         }
         if (this->getCd()->getV()[0].getType() == "") {
-            s = "please classify the data";
+            s = "please classify the data\n";
             this->getIO()->write(s);
             s = "!";
             this->getIO()->write(s);
